7369bc28c21f2fddcd5eeadd4132c1ef58ddc2ef
Tim-Rohner <timroh92@gmail.com>
2022-02-01 17:04:06 +0100

    adding recipes.js

---
+++ recipes.js
@@ -0,0 +1,9 @@
+const foodArray = ['Pizza', 'Burger', 'Salad', 'Doener', 'Pasta', 'Halloumi'];
+
+const randomFood = () => {
+    console.log(foodArray[Math.floor(Math.random()*6)])
+}
+
+randomFood()
+
+
