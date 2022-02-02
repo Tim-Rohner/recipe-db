00b313005ef2be8006e2af4f590a3c59e769ec6c
Tim-Rohner <timroh92@gmail.com>
2022-02-02 11:21:13 +0100

    Recipes.js now choses randomly from the JSON recipe-database

--- recipes.js
+++ recipes.js
@@ -1,9 +1,17 @@
-const foodArray = ['Pizza', 'Burger', 'Salad', 'Doener', 'Pasta', 'Halloumi'];
+/*const foodArray = ['Pizza', 'Burger', 'Salad', 'Doener', 'Pasta', 'Halloumi'];
 
 const randomFood = () => {
     console.log(foodArray[Math.floor(Math.random()*6)])
 }
 
 randomFood()
+*/
+const fs = require('./db-recipes.json') //needed to read JSON with node, assigns it to fs
 
+var randomProperty = function (fs) {
+    var keys = Object.keys(fs);
+    return fs[keys[keys.length * Math.random() << 0]].name;
+};
+
+console.log('Your recipe for tonight is:\n \n'+ randomProperty(fs) +"\n\nalternatively, we offer \n \n"+ randomProperty(fs)+"\n \n")
 
